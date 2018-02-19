d3353a567b636533c679c6e93c49d02c7455ab4c
sam <sam@mechanicadvisor.com>
2018-02-19 16:34:09 -0500

    Fist commit

---
+++ main.js
@@ -0,0 +1,38 @@
+
+var url = "https://api.mechanicadvisor.com/v7/schedule/";
+var key = "Zjc3Y2ZmNDUyYmI5NGZiOWE4OGI4YjgyYmNlMzE4NjI6";
+var shop = "param.shopId=576676";
+var year = "GetShopVehicleYears?";
+var selectyear = '';
+var make = "GetShopVehicleMakes?";
+var model = "GetShopVehicleModels?";
+var carContainer = document.getElementById("Cars-info");
+var btn = document.getElementById("btn");
+
+
+
+btn.addEventListener("click", function() {
+
+    var ourRequest = new XMLHttpRequest();
+
+    ourRequest.open('GET', url + year + shop);
+    ourRequest.setRequestHeader ('Authorization', 'Basic ' + key);
+    ourRequest.onload = function() {
+
+        var ourData =JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+      };
+      ourRequest.send();
+    });
+
+function renderHTML(data) {
+      var htmlString = "";
+
+      for (i = 0; i < data.length; i++) {
+
+         htmlString += "<p>" + data[i]; + "</p>";
+
+  }
+
+  carContainer.insertAdjacentHTML('beforeend', htmlString);
+}
