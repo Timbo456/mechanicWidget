14d338972ded4aee34052cad1af29d25be55024c
sam <samtaylor720@gmail.com>
2018-02-19 17:21:44 -0500

    Added some more api variables like shop hours, services, etc.

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ var url = "https://api.mechanicadvisor.com/v7/schedule/";
 var key = "Zjc3Y2ZmNDUyYmI5NGZiOWE4OGI4YjgyYmNlMzE4NjI6";
 var shop = "param.shopId=576676";
 var year = "GetShopVehicleYears?";
-var selectyear = '';
 var make = "GetShopVehicleMakes?";
 var model = "GetShopVehicleModels?";
+var service = "GetShopServices";
+var hours = "GetShopHours";
+var
 var carContainer = document.getElementById("Cars-info");
 var btn = document.getElementById("btn");
 
@@ -27,12 +29,9 @@ btn.addEventListener("click", function() {
 
 function renderHTML(data) {
       var htmlString = "";
-
       for (i = 0; i < data.length; i++) {
-
          htmlString += "<button>" + data[i]; + "</button>";
+      }
+      carContainer.insertAdjacentHTML('beforeend', htmlString);
 
-  }
-
-  carContainer.insertAdjacentHTML('beforeend', htmlString);
-}
+};
