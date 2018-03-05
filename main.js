e01e669622195b44b978d2a8ee7cddd68730ac82
Timbo456 <samtaylor720@gmail.com>
2018-03-04 22:47:00 -0500

    Got 'services' working and changed style

--- main.js
+++ main.js
@@ -3,80 +3,11 @@ function Scheduler(shopId) {
   this.shopId = shopId
   this.years = []
   this.makes = []
-  this.allMakes = [
-    {id: 3106, name: "Acura"},
-    {id: 3107, name: "Alfa romeo"},
-    {id: 3108, name: "Aston martin"},
-    {id: 3109, name: "Audi"},
-    {id: 3853, name: "Belmont"},
-    {id: 3112, name: "Bentley"},
-    {id: 3113, name: "Bmw"},
-    {id: 3967, name: "Boxtruck"},
-    {id: 3488, name: "Buell"},
-    {id: 3114, name: "Bugatti"},
-    {id: 3115, name: "Buick"},
-    {id: 4028, name: "Ca/on"},
-    {id: 3116, name: "Cadillac"},
-    {id: 4137, name: "CaSE"},
-    {id: 3117, name: "Chevrolet"},
-    {id: 3169, name: "Chevy"},
-    {id: 3118, name: "Chrysler"},
-    {id: 3119, name: "Dodge"},
-    {id: 3993, name: "Doolittle"},
-    {id: 3120, name: "Ferrari"},
-    {id: 3121, name: "Fiat"},
-    {id: 3123, name: "Ford"},
-    {id: 3776, name: "Freightliner"},
-    {id: 3124, name: "Gmc"},
-    {id: 3125, name: "Honda"},
-    {id: 3126, name: "Hummer"},
-    {id: 3127, name: "Hyundai"},
-    {id: 3128, name: "Infiniti"},
-    {id: 3129, name: "Isuzu"},
-    {id: 3130, name: "Jaguar"},
-    {id: 3131, name: "Jeep"},
-    {id: 3682, name: "Kawasaki"},
-    {id: 3835, name: "Kenworth"},
-    {id: 3132, name: "Kia"},
-    {id: 3133, name: "Lamborghini"},
-    {id: 3134, name: "Land rover"},
-    {id: 4079, name: "LANT"},
-    {id: 3135, name: "Lexus"},
-    {id: 3136, name: "Lincoln"},
-    {id: 3138, name: "Lotus"},
-    {id: 3139, name: "Maserati"},
-    {id: 3140, name: "Maybach"},
-    {id: 3141, name: "Mazda"},
-    {id: 3143, name: "Mercedes-benz"},
-    {id: 3144, name: "Mercury"},
-    {id: 3145, name: "Mini"},
-    {id: 3894, name: "Mitsu fuso"},
-    {id: 3146, name: "Mitsubishi"},
-    {id: 3147, name: "Nissan"},
-    {id: 3896, name: "Nissan-datsun"},
-    {id: 3150, name: "Pontiac"},
-    {id: 3151, name: "Porsche"},
-    {id: 3152, name: "Ram"},
-    {id: 3153, name: "Rolls-royce"},
-    {id: 3155, name: "Saab"},
-    {id: 3157, name: "Saturn"},
-    {id: 3159, name: "Scion"},
-    {id: 3161, name: "Smart"},
-    {id: 3162, name: "Subaru"},
-    {id: 3164, name: "Suzuki"},
-    {id: 3165, name: "Tesla"},
-    {id: 3166, name: "Toyota"},
-    {id: 3787, name: "Trailer"},
-    {id: 3876, name: "Ud"},
-    {id: 3167, name: "Volkswagen"},
-    {id: 3168, name: "Volvo"},
-    {id: 3794, name: "Workhorse"},
-    {id: 3976, name: "Zap"}
-  ]
   this.models = []
   this.selectedYear = null
   this.selectedModel = null
   this.selectedMake = null
+  this.selectedService = null
   this.services = []
   this.shopHours = null
   this.api = {
@@ -85,7 +16,8 @@ function Scheduler(shopId) {
     endpoints: {
       shopYears: "GetShopVehicleYears",
       makes: "GetShopVehicleMakes",
-      models: "GetShopVehicleModels"
+      models: "GetShopVehicleModels",
+      services: "GetShopServices"
     }
   }
   this.selection = {}
@@ -94,23 +26,19 @@ function Scheduler(shopId) {
 
     var that = this
 
-    this.years = this.getYears(function () {
-      that.selectedYear = that.years[0]
+      this.years = this.getYears(function () {
+        that.selectedYear = that.years[0]
 
       that.makes = that.getMakes(function () {
         that.selectedMake = that.makes[0].Id
 
-        // that.render()
-        // that.bindEvents()
+      that.models = that.getModels(function () {
+        that.selectedModel = that.models[0].Id
 
-        // FIX THIS DUMBASS
-        that.models = that.getModels(function () {
-          that.selectedModel = that.models[0].Id
+      this.services = that.getServices(function () {
+        that.selectedService = that.services[0].Id
 
-           // that.render()
-           // that.bindEvents()
-
-          // WHERE RENDER SHOULD BE
+        })
 
           that.render()
           that.bindEvents()
@@ -123,9 +51,10 @@ function Scheduler(shopId) {
   }.bind(this)
 
   this.render = function () {
-    var formContentEl = $("#form-content")
 
+    var formContentEl = $("#form-content")
     var yearSelectOptionsHtml = ""
+
     for (var i = 0; i < this.years.length; i++) {
       if (parseInt(this.selectedYear) === this.years[i]) {
         yearSelectOptionsHtml += `<option value=${this.years[i]} selected>${this.years[i]}</option>`
@@ -133,39 +62,52 @@ function Scheduler(shopId) {
       else {
         yearSelectOptionsHtml += `<option value=${this.years[i]}>${this.years[i]}</option>`
       }
-  }
-
+    }
 
     var yearSelectHtml = "<div class='select-wrap'><select id='year-select'>" + yearSelectOptionsHtml + "</select></div>"
-
     var makeSelectOptionsHtml = ""
 
     for (var i = 0; i < this.makes.length; i++) {
-      console.log(this.selectedMake)
-      console.log(this.makes[i].Id)
+      // console.log(this.selectedMake)
+      // console.log(this.makes[i].Id)
       if (parseInt(this.selectedMake) === this.makes[i].Id) {
-      makeSelectOptionsHtml += `<option value=${this.makes[i].Id} selected>${this.makes[i].Name}</option>`
+        makeSelectOptionsHtml += `<option value=${this.makes[i].Id} selected>${this.makes[i].Name}</option>`
+      }
+      else {
+        makeSelectOptionsHtml += `<option value=${this.makes[i].Id}>${this.makes[i].Name}</option>`
+      }
     }
-    else {
-      makeSelectOptionsHtml += `<option value=${this.makes[i].Id}>${this.makes[i].Name}</option>`
-    }
-  }
-    var makeSelectHtml = "<div class='select-wrap'><select id='make-select'>" + makeSelectOptionsHtml + "</select></div>"
 
+    var makeSelectHtml = "<div class='select-wrap'><select id='make-select'>" + makeSelectOptionsHtml + "</select></div>"
     var modelSelectOptionsHtml = ""
+
     for (var i = 0; i < this.models.length; i++) {
       if (this.selectedModel === this.models[i]) {
-      modelSelectOptionsHtml += `<option value= ${this.models[i].Id} selected>${this.models[i].Name}</option>`
-    } else {
-      modelSelectOptionsHtml += `<option value= ${this.models[i].Id}>${this.models[i].Name}</option>`
+        modelSelectOptionsHtml += `<option value= ${this.models[i].Id} selected>${this.models[i].Name}</option>`
+      }
+      else {
+        modelSelectOptionsHtml += `<option value= ${this.models[i].Id}>${this.models[i].Name}</option>`
+      }
     }
-}
 
     var modelSelectHtml = "<div class= 'select-wrap'><select id='model-select'>" + modelSelectOptionsHtml + "</select></div>"
+    var serviceSelectOptionsHtml = ""
 
-    var buttonHtml = "<button>Go</button>"
+    for (var i = 0; i < this.services.length; i++) {
+      if (this.selectedService === this.services[i]) {
+        serviceSelectOptionsHtml += `<option value= ${this.services[i].Id} selected>${this.services[i].Name}</option>`
+      }
+      else {
+        serviceSelectOptionsHtml += `<option value= ${this.services[i].Id}>${this.services[i].Name}</option>`
+      }
+    }
 
-    formContentEl.html(yearSelectHtml + makeSelectHtml + modelSelectHtml + buttonHtml)
+    var serviceSelectHtml = "<div class= 'select-wrap'><select id='services-select'>" + serviceSelectOptionsHtml + "</select></div>"
+
+
+    var buttonHtml = "<button>Schedule !</button>"
+
+    formContentEl.html(yearSelectHtml + makeSelectHtml + modelSelectHtml + serviceSelectHtml + buttonHtml)
 
 
   }.bind(this)
@@ -173,7 +115,8 @@ function Scheduler(shopId) {
   this.bindEvents = function () {
     var yearSelectEl = $("#year-select")
     var makeSelectEl = $("#make-select")
-    var modelSelectE1 = $("#model-select")
+    var modelSelectEl = $("#model-select")
+    var serviceSelectEl = $("#services-select")
     var that = this;
 
     yearSelectEl.change(function(e) {
@@ -197,10 +140,6 @@ function Scheduler(shopId) {
       })
     }.bind(this))
 
-    // modelSelectE1.change(function(e){
-    //   this.selectedModel = $(e.currentTarget).val()
-    //   //this.models = this.getModels()
-    // })
 
   }.bind(this)
 //////////////
@@ -248,19 +187,21 @@ function Scheduler(shopId) {
     modelsRequest.send()
 
   }.bind(this)
+/////////////
+  this.getServices = function(callback) {
+    var servicesURL = this.api.url + this.api.endpoints.services + "?param.shopId=" + this.shopId
+    var serviceRequest = new XMLHttpRequest()
+    serviceRequest.open("GET", servicesURL)
+    serviceRequest.setRequestHeader("Authorization", "Basic " + this.api.key)
+    serviceRequest.onload = function() {
+      console.log(serviceRequest.response)
+      this.services = JSON.parse(serviceRequest.response)
+      if (callback) callback()
+    }.bind(this)
 
-  // this.getModels = function () {
-  //   var shopYearsUrl = this.api.url + this.api.endpoints.shopYears + '?param.shopId=' + this.shopId
-  //   var yearsRequest = new XMLHttpRequest()
-  //   yearsRequest.open("GET", shopYearsUrl)
-  //   yearsRequest.setRequestHeader("Authorization", "Basic " + this.api.key)
-  //   yearsRequest.onload = function(data) {
-  //       this.years = data
-  //   }.bind(this)
-  //
-  //   yearsRequest.send()
-  //
-  // }.bind(this)
+    serviceRequest.send()
+
+  }.bind(this)
 }
 
 $(function() {
